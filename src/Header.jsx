4ab6ea848e29a02d7dd953acf63295c94349113b
MaksimKing <artemov99@list.ru>
2020-07-25 21:32:41 +0300

    Lesson number 1

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Header() {
+    return (
+        <header>
+            <h1>Reddit fot our own user. I am MAKS!!!</h1>
+        </header>
+    )
+}
