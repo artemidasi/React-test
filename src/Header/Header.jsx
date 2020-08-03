fe3790e12b1304e89a111f75b7d9a7f1938df8a1
MaksimKing <artemov99@list.ru>
2020-08-03 19:04:08 +0300

    upgrade branch HMR_SSR

---
+++ src/Header/Header.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Header() {
+    return (
+        <header>
+            <h1>Reddit fot our own user. I am MAKSIM GOD!!!</h1>
+        </header>
+    )
+}
