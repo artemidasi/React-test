fa9a2d18d5f34a1c0a38e200933e6df1edccff40
MaksimKing <artemov99@list.ru>
2020-08-03 12:56:17 +0300

    update branch master

--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export function Header() {
-    return (
-        <header>
-            <h1>Reddit fot our own user. I am MAKS!!!</h1>
-        </header>
-    )
-}
