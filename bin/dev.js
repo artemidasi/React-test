dcd3957895d4e701cdae43d559fa05a4c10a8348
Maks <59100893+Xokage123@users.noreply.github.com>
2020-10-13 18:04:01 +0300

    configuring_the_repository

--- bin/dev.js
+++ bin/dev.js
@@ -1,5 +1,5 @@
 const webpack = require("webpack");
-const [webpackClientConfig,webpackServerConfig] = require("../webpack.config");
+const [webpackClientConfig, webpackServerConfig] = require("../webpack.config");
 const nodemon = require("nodemon");
 const path = require("path");
 const webpackDevMiddleware = require("webpack-dev-middleware");
@@ -10,44 +10,44 @@ const hmrServer = express();
 const clientCpmpiler = webpack(webpackClientConfig);
 
 hmrServer.use(webpackDevMiddleware(clientCpmpiler, {
-  publicPath: webpackClientConfig.output.publicPath,
-  serverSideRender: true,
-  noInfo: true,
-  watchOptions: {
-    ignore: /dist/,
-  },
-  writeToDisk: true,
-  stats: "errors-only"
+    publicPath: webpackClientConfig.output.publicPath,
+    serverSideRender: true,
+    noInfo: true,
+    watchOptions: {
+        ignore: /dist/,
+    },
+    writeToDisk: true,
+    stats: "errors-only"
 }));
 
 hmrServer.use(webpackHotMiddleware(clientCpmpiler, {
-  path: '/static/__webpack_hmr',
+    path: '/static/__webpack_hmr',
 }));
 
 hmrServer.listen(3001, () => {
-  console.log("HMR server succesfuly started");
+    console.log("HMR server succesfuly started");
 });
 
 const compiler = webpack(webpackServerConfig);
 
 compiler.run((err) => {
-  if (err) {
-    console.log(`Compilation failed: ${err}`);
-  }
-
-  compiler.watch({}, (err) => {
     if (err) {
-      console.log(`Compilation failed: ${err}`);
+        console.log(`Compilation failed: ${err}`);
     }
 
-    console.log("Compilation was succesfully");
-  });
+    compiler.watch({}, (err) => {
+        if (err) {
+            console.log(`Compilation failed: ${err}`);
+        }
 
-  nodemon({
-    script: path.resolve(__dirname, "../dist/server/server.js"),
-    watch: [
-      path.resolve(__dirname, "../dist/server"),
-      path.resolve(__dirname, "../dist/client"),
-    ]
-  })
+        console.log("Compilation was succesfully");
+    });
+
+    nodemon({
+        script: path.resolve(__dirname, "../dist/server/server.js"),
+        watch: [
+            path.resolve(__dirname, "../dist/server"),
+            path.resolve(__dirname, "../dist/client"),
+        ]
+    })
 });
